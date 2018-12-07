bd59e886301db12f5418e04d2c90e432d7547631
Kobi David <kobi5252@gmail.com>
2018-12-08 00:19:25 +0200

    Initial commit

---
+++ NeedU/src/main/java/com/needyou/NeedU/NeedUApplication.java
@@ -0,0 +1,12 @@
+package com.needyou.NeedU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedUApplication.class, args);
+	}
+}
